19ae357cb54a7bdc6d19429d84465a370f0a919f
Hassan <hassanabbas93@hotmail.com>
2018-12-21 18:21:45 +0000

    searchbox state
    working on search change state

---
+++ src/SearchBox.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const SearchBox = () => {
+    return (
+        <div>
+            <input 
+                className='ps3 ba b--green bg-lightest-blue'
+                type='search' 
+                placeholder='search robots' 
+            />
+
+        </div>
+    );
+
+}
+
+export default SearchBox;
