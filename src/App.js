6841982f419827bd6328945fceb7648cdc1b0df5
hassan <hassanabbas93@hotmail.com>
2018-12-24 11:00:27 +0000

    on change set searchfield state which gets filtered robots

--- src/App.js
+++ src/App.js
@@ -12,16 +12,20 @@ class App extends Component {
         }
     }
 
-    onSearchChange(event){
-        console.log(event);
+    onSearchChange = (event) => {
+        this.setState({searchfield: event.target.value})
     }
 
     render(){
+        const filteredRobots = this.state.robots.filter(robots =>{
+            return robots.name.toLowerCase().include(this.state.searchfield.toLowerCase())
+        })
+      
         return(
             <div className='tc'>
                 <h1>RoboFriends</h1>
-                <SearchBox searchChange={onSearchChange} />
-                <CardList robots={robots} />
+                <SearchBox searchChange={this.onSearchChange} />
+                <CardList robots={filteredRobots} />
             </div>
          
         );
--- src/SearchBox.js
+++ src/SearchBox.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
-const SearchBox = () => {
+const SearchBox = ({searchfield, seachChange}) => {
     return (
         <div>
             <input 
                 className='ps3 ba b--green bg-lightest-blue'
                 type='search' 
                 placeholder='search robots' 
+                onChange={seachChange}
             />
 
         </div>
