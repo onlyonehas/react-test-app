155a2bb47ba9f8bd04ca1198e0400da6bedcda73
onlyonehas <hassanabbas93@hotmail.com>
2019-01-03 10:02:25 +0000

    Updates

---
+++ precache-manifest.43131c6412df8cf8a5edfbb4a97e3448.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/react-test-app/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "5b594a7113d6455c9a95",
+    "url": "/react-test-app/static/js/runtime~main.5b594a71.js"
+  },
+  {
+    "revision": "2057f24f7c7d494e8e11",
+    "url": "/react-test-app/static/js/main.2057f24f.chunk.js"
+  },
+  {
+    "revision": "37c49579b12c4a1d04f2",
+    "url": "/react-test-app/static/js/1.37c49579.chunk.js"
+  },
+  {
+    "revision": "2057f24f7c7d494e8e11",
+    "url": "/react-test-app/static/css/main.89e9e300.chunk.css"
+  },
+  {
+    "revision": "37c49579b12c4a1d04f2",
+    "url": "/react-test-app/static/css/1.71784eb0.chunk.css"
+  },
+  {
+    "revision": "dd3eeaa9ecf8d03ce7f8e4e5c7e295fe",
+    "url": "/react-test-app/index.html"
+  }
+];
